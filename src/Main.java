bb09cbad4f09c956713a85652aa08694a2961a52
Michel Lammertink <michellammertink@gmail.com>
2020-03-21 18:36:24 +0100

    Fixed typo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         /* The traversal implementations internally use a List in
-         * order to be able to be able to predict the outcome of the algorithm.
+         * order to be able to predict the outcome of the algorithm.
          * Normally it is more appropriate to use a Set instead of a
          * List to store the neighbours of a node.
          */
